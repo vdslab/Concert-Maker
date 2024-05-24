6a4bd2cd2a258ac0d19d67be350427e0163603d4
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2024-05-24 15:17:52 +0900

    MUIとReact RouterDOMの導入

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+]);
+
+export const themeOptions = {
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#263238",
+    },
+    secondary: {
+      main: "#ffa000",
+    },
+    background: {
+      default: "#eeeeee",
+    },
+  },
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <ThemeProvider theme={themeOptions}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </React.StrictMode>,
-)
+);
