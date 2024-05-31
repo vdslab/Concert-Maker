7c310a6d2f458cc27e2aa155328ebd6a7802a590
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2024-05-31 17:34:52 +0900

    feat: Add MyConcertCard component and update MyConcert page

---
+++ src/components/layouts/MyConcertCard.jsx
@@ -0,0 +1,57 @@
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+
+export default function MyConcertCard() {
+  return (
+    <Card elevation={3}>
+      <Box sx={{ p: 2 }}>
+        <Grid
+          container
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+        >
+          <Grid item>
+            <Stack
+              container
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center"
+              spacing={2}
+            >
+              <Button variant="contained" color="secondary" size="small">
+                Main
+              </Button>
+              <Typography gutterBottom variant="h5" component="div">
+                My Concert 1
+              </Typography>
+            </Stack>
+          </Grid>
+
+          <Grid item>
+            <Typography gutterBottom variant="h6" component="div">
+              1h 30m
+            </Typography>
+          </Grid>
+        </Grid>
+      </Box>
+      <Divider />
+      <Box sx={{ p: 2 }}>
+        <Typography gutterBottom variant="body2">
+          Select type
+        </Typography>
+        <Stack direction="row" spacing={1}>
+          <Chip color="primary" label="Soft" size="small" />
+          <Chip label="Medium" size="small" />
+          <Chip label="Hard" size="small" />
+        </Stack>
+      </Box>
+    </Card>
+  );
+}
--- src/pages/MyConcert.jsx
+++ src/pages/MyConcert.jsx
@@ -1,5 +1,12 @@
+import MyConcertCard from "@/components/layouts/MyConcertCard.jsx";
+
 function MyConcert() {
-  return <h1>My Concert</h1>;
+  return (
+    <>
+      <h1>My 演奏会</h1>
+      <MyConcertCard />
+    </>
+  );
 }
 
 export default MyConcert;
