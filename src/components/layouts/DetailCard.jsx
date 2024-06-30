e7c9664d3e31381c464cbd472d65e94f4fc09f47
Ishi <syi378039@gmail.com>
2024-07-01 01:21:15 +0900

    書く音楽配信サービスのボタンを追加

--- src/components/layouts/DetailCard.jsx
+++ src/components/layouts/DetailCard.jsx
@@ -5,9 +5,14 @@ import {
   AccordionDetails,
   Typography,
   Grid,
+  Button,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Works from "../../assets/works_v02.json";
+import Works from "../../assets/works_v03.json";
+import AppleMusic from "../../assets/Apple_Music_Icon.svg";
+import Spotify from "../../assets/Spotify_Icon.png";
+import YouTube from "../../assets/YouTube_Music.png";
+import AmazonMusic from "../../assets/Amazon_Music.png";
 
 const DetailCard = ({ clicknode }) => {
   let Data = null;
@@ -17,6 +22,25 @@ const DetailCard = ({ clicknode }) => {
   const handleButtonClick = (event) => {
     event.stopPropagation();
     console.log(Data);
+  };
+
+  const buttonStyle = {
+    borderRadius: "50%",
+    width: "60px",
+    height: "60px",
+    minWidth: "60px",
+    padding: 0,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    marginBottom: "8px",
+  };
+
+  const imageStyle = {
+    width: "70%",
+    height: "70%",
+    objectFit: "contain",
   };
 
   return (
@@ -35,8 +59,9 @@ const DetailCard = ({ clicknode }) => {
               演奏時間：{Data.duration}分
             </Typography>
             <Typography sx={{ color: "text.secondary" }}>
-              未対応 Vn.I: 14, Vn.II: 12, Va.: 10, Vc.: 8, Cb: 6, Fl.: 2, Ob.:
-              2, Cl.: 2, Bsn.:3[1.2.cbn], Hrn.: 4, Trp.: 2, Trb.: 3, Tub.: 0
+              {/* 未対応 Vn.I: 14, Vn.II: 12, Va.: 10, Vc.: 8, Cb: 6, Fl.: 2, Ob.:
+              2, Cl.: 2, Bsn.:3[1.2.cbn], Hrn.: 4, Trp.: 2, Trb.: 3, Tub.: 0 */}
+              {Data.workFormulaStr}
             </Typography>
             <button onClick={handleButtonClick}>登録ボタン実装予定</button>
           </Grid>
@@ -53,6 +78,64 @@ const DetailCard = ({ clicknode }) => {
                     {Data.workMovementDuration[index]}
                   </Typography>
                 </Grid>
+              </Grid>
+            ))}
+          </Grid>
+          <Grid item xs={12}>
+            <Typography>聴く</Typography>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            container
+            spacing={2}
+            justifyContent="center"
+            alignItems="flex-start"
+          >
+            {[
+              {
+                name: "Spotify",
+                icon: Spotify,
+                url: "https://open.spotify.com/",
+              },
+              {
+                name: "YouTube",
+                icon: YouTube,
+                url: "https://music.youtube.com/",
+              },
+              {
+                name: "Apple Music",
+                icon: AppleMusic,
+                url: "https://music.apple.com/jp/",
+              },
+              {
+                name: "Amazon Music",
+                icon: AmazonMusic,
+                url: "https://music.amazon.co.jp/",
+              },
+            ].map((service) => (
+              <Grid
+                key={service.name}
+                item
+                xs={3}
+                container
+                direction="column"
+                alignItems="center"
+              >
+                <Button
+                  variant="contained"
+                  sx={buttonStyle}
+                  onClick={() => window.open(service.url)}
+                >
+                  <img
+                    src={service.icon}
+                    alt={service.name}
+                    style={imageStyle}
+                  />
+                </Button>
+                <Typography align="center" variant="caption">
+                  {service.name}
+                </Typography>
               </Grid>
             ))}
           </Grid>
