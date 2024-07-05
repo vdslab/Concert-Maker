a3300b0d360fdd554c44d68059554f2d1f07d204
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2024-07-05 15:48:13 +0900

    feat: Add search tabs for graph, list, and favorites

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -8,14 +8,87 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="left-half">
-          <NodeLinkDiagram setClicknode={setClicknode} />
-        </div>
+        <SearchTabs setClicknode={setClicknode} />
         <div className="right-half">
           <DetailCard clicknode={clicknode} />
         </div>
       </div>
     </>
+  );
+}
+
+import * as React from "react";
+import PropTypes from "prop-types";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+SearchTabs.propTypes = {
+  setClicknode: PropTypes.func.isRequired,
+};
+
+function SearchTabs({ setClicknode }) {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "65%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+          centered
+        >
+          <Tab label="グラフ" {...a11yProps(0)} />
+          <Tab label="一覧" {...a11yProps(1)} />
+          <Tab label="お気に入り" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        {/* <div className="left-half"> */}
+        <NodeLinkDiagram setClicknode={setClicknode} />
+        {/* </div> */}
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        一覧
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        お気に入り
+      </CustomTabPanel>
+    </Box>
   );
 }
 
