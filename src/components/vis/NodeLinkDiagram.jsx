4222aaf6e26443c4328cb9768f2c679a0d6834fc
Ishi <syi378039@gmail.com>
2024-07-16 08:35:11 +0900

    曲詳細の横幅を修正

--- src/components/vis/NodeLinkDiagram.jsx
+++ src/components/vis/NodeLinkDiagram.jsx
@@ -13,27 +13,49 @@ import SpotifyIcon from "../../assets/Spotify_Icon.png";
 import YoutubeIcon from "../../assets/YouTube_Music.png";
 import AmazonIcon from "../../assets/Amazon_Music.png";
 import AppleIcon from "../../assets/Apple_Music_Icon.svg";
-import { Button, ButtonGroup } from "@mui/material";
-import * as d3 from "d3";
 import MyConcert from "@/utils/myConcert";
-
+import * as d3 from "d3";
+import {
+  Button,
+  ButtonGroup,
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  Box,
+  IconButton,
+  Menu,
+  MenuItem,
+  Grid,
+} from "@mui/material";
+import { styled } from "@mui/system";
+import CloseIcon from "@mui/icons-material/Close";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
-import ClickAwayListener from "@mui/material/ClickAwayListener";
-import Grow from "@mui/material/Grow";
-import Paper from "@mui/material/Paper";
-import Popper from "@mui/material/Popper";
-import MenuItem from "@mui/material/MenuItem";
-import MenuList from "@mui/material/MenuList";
+
+const StyledPaper = styled(Paper)(({ theme }) => ({
+  position: "absolute",
+  top: "30px",
+  left: "10px",
+  width: "400px",
+  height: "calc(100vh - 100px)",
+  maxHeight: "calc(100vh - 100px)",
+  overflowY: "auto",
+}));
+
+const StyledIconButton = styled(IconButton)(({ theme }) => ({
+  position: "absolute",
+  top: "10px",
+  right: "10px",
+}));
 
 function SplitButton({ songId }) {
-  console.log(songId);
-
   const concertNames = MyConcert.getConcerts().map((concert) => concert.name);
   if (concertNames.length === 0) {
     MyConcert.createConcert("My演奏会");
     concertNames.push("My演奏会");
   }
-  console.log(concertNames);
 
   const [open, setOpen] = React.useState(false);
   const anchorRef = React.useRef(null);
@@ -51,9 +73,10 @@ function SplitButton({ songId }) {
     setOpen(false);
   };
 
-  const handleMenuItemClick = (event) => {
+  const handleMenuItemClick = (event, concertName) => {
     event.preventDefault();
     event.stopPropagation();
+    MyConcert.saveWork(songId, concertName);
     handleClose(event);
   };
 
@@ -63,9 +86,7 @@ function SplitButton({ songId }) {
         variant="contained"
         ref={anchorRef}
         aria-label="split button"
-        onClick={(e) => e.stopPropagation()}
       >
-        {/* Amazonといっしょにしました。 */}
         <Button
           onClick={(e) => {
             MyConcert.saveWork(songId, concertNames[0]);
@@ -78,48 +99,27 @@ function SplitButton({ songId }) {
           size="small"
           aria-controls={open ? "split-button-menu" : undefined}
           aria-expanded={open ? "true" : undefined}
-          aria-label="select merge strategy"
           aria-haspopup="menu"
           onClick={handleToggle}
         >
           <ArrowDropDownIcon />
         </Button>
       </ButtonGroup>
-      <Popper
-        open={open}
+      <Menu
+        id="split-button-menu"
         anchorEl={anchorRef.current}
-        role={undefined}
-        transition
-        disablePortal
+        open={open}
+        onClose={handleClose}
       >
-        {({ TransitionProps, placement }) => (
-          <Grow
-            {...TransitionProps}
-            style={{
-              transformOrigin:
-                placement === "bottom" ? "center top" : "center bottom",
-            }}
+        {concertNames.map((concertName) => (
+          <MenuItem
+            key={concertName}
+            onClick={(event) => handleMenuItemClick(event, concertName)}
           >
-            <Paper>
-              <ClickAwayListener onClickAway={handleClose}>
-                <MenuList id="split-button-menu">
-                  {concertNames.map((concertName) => (
-                    <MenuItem
-                      key={concertName}
-                      onClick={
-                        (MyConcert.saveWork(songId, concertName),
-                        handleMenuItemClick)
-                      }
-                    >
-                      {concertName}
-                    </MenuItem>
-                  ))}
-                </MenuList>
-              </ClickAwayListener>
-            </Paper>
-          </Grow>
-        )}
-      </Popper>
+            {concertName}
+          </MenuItem>
+        ))}
+      </Menu>
     </React.Fragment>
   );
 }
@@ -222,10 +222,141 @@ const allPlayedWithWorkIds = new Set(
   ])
 );
 
+const NodeInfo = ({ node, onClose }) => {
+  if (!node) return null;
+
+  const musicServices = [
+    {
+      name: "Spotify",
+      icon: SpotifyIcon,
+      url: "https://open.spotify.com/search/",
+    },
+    {
+      name: "YouTube Music",
+      icon: YoutubeIcon,
+      url: "https://music.youtube.com/search?q=",
+    },
+    {
+      name: "Apple Music",
+      icon: AppleIcon,
+      url: "https://music.apple.com/search?term=",
+    },
+    {
+      name: "Amazon Music",
+      icon: AmazonIcon,
+      url: "https://music.amazon.com/search/",
+    },
+  ];
+
+  const relatedWorks = [
+    {
+      title: node.title,
+      composer: node.composer,
+      duration: "33分",
+      id: "2022-2200-tmp-str",
+    },
+    {
+      title: node.title,
+      composer: node.composer,
+      duration: "33分",
+      id: "2022-2200-tmp-str",
+    },
+    {
+      title: node.title,
+      composer: node.composer,
+      duration: "33分",
+      id: "2022-2200-tmp-str",
+    },
+  ];
+
+  return (
+    <StyledPaper>
+      <Box p={2}>
+        <StyledIconButton onClick={onClose}>
+          <CloseIcon />
+        </StyledIconButton>
+        <Typography variant="h5" gutterBottom>
+          {node.title}
+        </Typography>
+        <Typography variant="body2" gutterBottom>
+          演奏時間: 33分
+        </Typography>
+        <Typography variant="body2" color="textSecondary" gutterBottom>
+          {node.workFormulaStr}
+        </Typography>
+        <SplitButton songId={node.id} />
+      </Box>
+      <Divider />
+      <Box p={2}>
+        <Typography variant="h6" gutterBottom>
+          詳細情報
+        </Typography>
+        <List>
+          {[
+            "I. Un poco sostenuto; Allegro",
+            "II. Andante sostenuto",
+            "III. Un poco allegretto e grazioso",
+            "IV. Adagio; Più andante; Allegro non troppo, ma con brio",
+          ].map((movement, index) => (
+            <ListItem key={index} dense>
+              <ListItemText
+                primary={movement}
+                secondary={
+                  index === 0
+                    ? "13分"
+                    : index === 1
+                    ? "10分"
+                    : index === 2
+                    ? "5分"
+                    : "17分"
+                }
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      <Divider />
+      <Box p={2}>
+        <Typography variant="h6" gutterBottom>
+          聴く
+        </Typography>
+        <Grid container spacing={2} justifyContent="space-between">
+          {musicServices.map((service, index) => (
+            <Grid item key={index}>
+              <Box display="flex" flexDirection="column" alignItems="center">
+                <img
+                  src={service.icon}
+                  alt={service.name}
+                  style={{ width: 40, height: 40, marginBottom: 5 }}
+                />
+                <Typography variant="caption">{service.name}</Typography>
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+      <Divider />
+      <Box p={2}>
+        <Typography variant="h6" gutterBottom>
+          よく一緒に演奏されている曲
+        </Typography>
+        {relatedWorks.map((work, index) => (
+          <Box key={index} mb={2}>
+            <Typography variant="subtitle1">{work.title}</Typography>
+            <Typography variant="body2">{work.composer}</Typography>
+            <Typography variant="body2" color="textSecondary">
+              演奏時間: {work.duration} | {work.id}
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+    </StyledPaper>
+  );
+};
+
 const NodeLinkDiagram = ({ setClicknode, setData }) => {
   const [clickedNode, setClickedNode] = useState(null);
   const fgRef = useRef();
-  console.log(clickedNode);
 
   const filteredWorks = useMemo(
     () => matchedDataByIds.filter((work) => allPlayedWithWorkIds.has(work.id)),
@@ -311,152 +442,8 @@ const NodeLinkDiagram = ({ setClicknode, setData }) => {
     setClicknode(null);
   }, [setClicknode]);
 
-  const NodeInfo = ({ node, onClose }) => {
-    if (!node) return null;
-
-    const musicServices = [
-      { name: "Spotify", icon: SpotifyIcon },
-      { name: "YouTube Music", icon: YoutubeIcon },
-      { name: "Apple Music", icon: AmazonIcon },
-      { name: "Amazon Music", icon: AppleIcon },
-    ];
-
-    const relatedWorks = [
-      {
-        title: node.title,
-        composer: node.composer,
-        duration: "33分",
-        id: "2022-2200-tmp-str",
-      },
-      {
-        title: node.title,
-        composer: node.composer,
-        duration: "33分",
-        id: "2022-2200-tmp-str",
-      },
-      {
-        title: node.title,
-        composer: node.composer,
-        duration: "33分",
-        id: "2022-2200-tmp-str",
-      },
-    ];
-
-    return (
-      <div
-        style={{
-          position: "absolute",
-          top: "30px",
-          left: "10px",
-          background: "white",
-          borderRadius: "10px",
-          boxShadow: "0 0 10px rgba(0,0,0,0.1)",
-          width: "300px",
-          maxHeight: "calc(100vh - 60px)",
-          overflowY: "auto",
-          fontFamily: "Arial, sans-serif",
-        }}
-      >
-        <button
-          onClick={onClose}
-          style={{
-            position: "sticky",
-            top: "10px",
-            right: "10px",
-            background: "none",
-            border: "none",
-            fontSize: "20px",
-            cursor: "pointer",
-            float: "right",
-            zIndex: 1,
-          }}
-        >
-          ×
-        </button>
-        <div style={{ padding: "20px" }}>
-          <h2 style={{ margin: "0 0 10px 0" }}>{node.title}</h2>
-          <p style={{ margin: "0 0 5px 0" }}>演奏時間: 33分</p>
-          <p style={{ margin: "0 0 15px 0", color: "#666" }}>
-            {node.workFormulaStr}
-          </p>
-          {/* <button
-            style={{
-              background: "#4285F4",
-              color: "white",
-              border: "none",
-              padding: "8px 15px",
-              borderRadius: "5px",
-              cursor: "pointer",
-            }}
-          >
-            演奏会に追加 ▼
-          </button> */}
-          <SplitButton songId={node.id} />
-        </div>
-        <div style={{ borderTop: "1px solid #eee", padding: "20px" }}>
-          <h3 style={{ margin: "0 0 10px 0" }}>詳細情報</h3>
-          <ul style={{ padding: 0, margin: 0, listStyle: "none" }}>
-            {[
-              "I. Un poco sostenuto; Allegro",
-              "II. Andante sostenuto",
-              "III. Un poco allegretto e grazioso",
-              "IV. Adagio; Più andante; Allegro non troppo, ma con brio",
-            ].map((movement, index) => (
-              <li
-                key={index}
-                style={{
-                  display: "flex",
-                  justifyContent: "space-between",
-                  marginBottom: "5px",
-                }}
-              >
-                <span>{movement}</span>
-                <span>
-                  {index === 0
-                    ? "13分"
-                    : index === 1
-                    ? "10分"
-                    : index === 2
-                    ? "5分"
-                    : "17分"}
-                </span>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div style={{ borderTop: "1px solid #eee", padding: "20px" }}>
-          <h3 style={{ margin: "0 0 10px 0" }}>聴く</h3>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            {musicServices.map((service, index) => (
-              <div key={index} style={{ textAlign: "center" }}>
-                <img
-                  src={service.icon}
-                  alt={service.name}
-                  style={{ width: "40px", height: "40px", marginBottom: "5px" }}
-                />
-                <p style={{ margin: 0, fontSize: "12px" }}>{service.name}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-        <div style={{ borderTop: "1px solid #eee", padding: "20px" }}>
-          <h3 style={{ margin: "0 0 10px 0" }}>よく一緒に演奏されている曲</h3>
-          {relatedWorks.map((work, index) => (
-            <div key={index} style={{ marginBottom: "10px" }}>
-              <h4 style={{ margin: "0 0 5px 0" }}>{work.title}</h4>
-              <p style={{ margin: "0 0 5px 0" }}>{work.composer}</p>
-              <p style={{ margin: "0", color: "#666" }}>
-                演奏時間: {work.duration} | {work.id}
-              </p>
-            </div>
-          ))}
-        </div>
-      </div>
-    );
-  };
-
   return (
-    <div ref={parentDivRef} style={{ position: "relative", height: "100%" }}>
+    <Box ref={parentDivRef} position="relative" height="100%">
       <ForceGraph2D
         ref={fgRef}
         graphData={data}
@@ -470,7 +457,7 @@ const NodeLinkDiagram = ({ setClicknode, setData }) => {
         onNodeClick={handleNodeClick}
       />
       <NodeInfo node={clickedNode} onClose={handleCloseInfo} />
-    </div>
+    </Box>
   );
 };
 
