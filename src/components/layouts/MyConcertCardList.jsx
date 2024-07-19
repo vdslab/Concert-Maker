4f3e16a3c9f474c2548ad3bfed02be481ec5c920
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2024-07-19 16:17:25 +0900

    refactor: Remove unused useEffect hook in MyConcertCardList component

--- src/components/layouts/MyConcertCardList.jsx
+++ src/components/layouts/MyConcertCardList.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 
 import MyConcertCard from "@/components/layouts/MyConcert/Card";
