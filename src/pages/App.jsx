725d846ba5fcc4c61d5a5226ecb7ffbb3872fb89
Ishi <syi378039@gmail.com>
2024-07-19 15:37:51 +0900

    Dataのstateとクリックしたノードのstateを親階層に移動

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,16 +1,24 @@
 import "./App.css";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import SearchBox from "@/components/layouts/SearchBox";
 import NodeLinkDiagram from "../components/vis/NodeLinkDiagram";
 import MyConcertCardList from "@/components/layouts/MyConcertCardList";
 
 function App() {
+  const [clicknode, setClicknode] = useState(null);
+  const [Data, setData] = useState([]);
   return (
     <>
       <div className="container">
         <Box width={2 / 3} className="left-half" sx={{ position: "relative" }}>
           <SearchBox />
-          <NodeLinkDiagram />
+          <NodeLinkDiagram
+            clicknode={clicknode}
+            setClicknode={setClicknode}
+            Data={Data}
+            setData={setData}
+          />
         </Box>
         <Box width={1 / 3} className="right-half" sx={{ overflow: "auto" }}>
           <MyConcertCardList />
