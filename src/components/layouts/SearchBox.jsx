bf451deb5b931dee37aa05763c5492bf38731137
Ishi <syi378039@gmail.com>
2024-07-23 08:54:52 +0900

    プルリク時のミスを修正

--- src/components/layouts/SearchBox.jsx
+++ src/components/layouts/SearchBox.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from "react";
 import Paper from "@mui/material/Paper";
-import Autocomplete from "@mui/material/Autocomplete";
-import TextField from "@mui/material/TextField";
+import InputBase from "@mui/material/InputBase";
 import Divider from "@mui/material/Divider";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
@@ -10,7 +8,7 @@ import FilterDialog from "@/components/layouts/FilterDialog";
 
 /**
  * 検索ボックスのコンポーネントです。
- * 
+ *
  * @param {Object} props - コンポーネントのpropsを格納するオブジェクトです。
  * @param {Object} props.Data - ネットワークのノードリンクデータを保持するstate変数のオブジェクトです。
  * @param {function} props.setData - state変数Dataを更新する関数です。
@@ -20,7 +18,6 @@ export default function SearchBox({ Data, setData }) {
   return (
     <Paper
       component="form"
-      onSubmit={handleSubmit}
       sx={{
         p: "2px 4px",
         display: "flex",
@@ -33,27 +30,15 @@ export default function SearchBox({ Data, setData }) {
         zIndex: 1,
       }}
     >
-      <Autocomplete
-        freeSolo
-        options={options}
-        onInputChange={handleInputChange}
-        fullWidth
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            placeholder="作品を検索する"
-            variant="standard"
-            InputProps={{
-              ...params.InputProps,
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-              disableUnderline: true,
-            }}
-          />
-        )}
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="作品を検索する"
+        inputProps={{ "aria-label": "作品を検索する" }}
+        startAdornment={
+          <InputAdornment position="start">
+            <SearchIcon />
+          </InputAdornment>
+        }
       />
       <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
       <FilterDialog Data={Data} setData={setData} />
