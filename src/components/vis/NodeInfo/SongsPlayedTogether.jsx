b9e6babb7b8db21b95603c9bd3e53bca96f20caf
aramackie <mackie1067@gmail.com>
2024-07-26 15:18:45 +0900

    SongPlayedTogetherにkeyを追加

--- src/components/vis/NodeInfo/SongsPlayedTogether.jsx
+++ src/components/vis/NodeInfo/SongsPlayedTogether.jsx
@@ -26,49 +26,48 @@ const SongPlayedTogether = ({ node, Data, setClicknode }) => {
         よく一緒に演奏されている曲
       </Typography>
       {linkNodesArray.map((work, index) => (
-        <>
-          <Button
-            onClick={() => {
-              setClicknode(work);
-            }}
-            sx={{
-              display: "block",
-              width: "100%",
-              textAlign: "left",
-              padding: "8px",
-              mb: 2,
-              textTransform: "none",
-              "&:hover": {
-                backgroundColor: "rgba(0, 0, 0, 0.04)",
-              },
-              "&:focus": {
-                outline: "none",
-                backgroundColor: "rgba(0, 0, 0, 0.08)",
-              },
-            }}
+        <Button
+          key={index}
+          onClick={() => {
+            setClicknode(work);
+          }}
+          sx={{
+            display: "block",
+            width: "100%",
+            textAlign: "left",
+            padding: "8px",
+            mb: 2,
+            textTransform: "none",
+            "&:hover": {
+              backgroundColor: "rgba(0, 0, 0, 0.04)",
+            },
+            "&:focus": {
+              outline: "none",
+              backgroundColor: "rgba(0, 0, 0, 0.08)",
+            },
+          }}
+        >
+          <Typography variant="body2" component="div">
+            {work.composer}
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            component="div"
+            sx={{ fontWeight: "bold" }}
           >
-            <Typography variant="body2" component="div">
-              {work.composer}
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              component="div"
-              sx={{ fontWeight: "bold" }}
-            >
-              {work.title}
-            </Typography>
-            <Typography variant="body2" component="div" color="text.secondary">
-              {work.duration === null
-                ? ""
-                : "演奏時間: " + work.duration + "分"}
-            </Typography>
-            <Typography variant="body2" component="div" color="text.secondary">
-              {work.workFormulaStr === null
-                ? ""
-                : "楽器編成: " + work.workFormulaStr.replace(/\n/g, " / ")}
-            </Typography>
-          </Button>
-        </>
+            {work.title}
+          </Typography>
+          <Typography variant="body2" component="div" color="text.secondary">
+            {work.duration === null
+              ? ""
+              : "演奏時間: " + work.duration + "分"}
+          </Typography>
+          <Typography variant="body2" component="div" color="text.secondary">
+            {work.workFormulaStr === null
+              ? ""
+              : "楽器編成: " + work.workFormulaStr.replace(/\n/g, " / ")}
+          </Typography>
+        </Button>
       ))}
     </Box>
   );
