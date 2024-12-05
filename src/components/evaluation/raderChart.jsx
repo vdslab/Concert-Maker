5cc95038a633e68a6a569dfc868c336646c6eb18
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2024-12-05 15:23:36 +0900

    いらないLegendsを削除

--- src/components/evaluation/raderChart.jsx
+++ src/components/evaluation/raderChart.jsx
@@ -63,27 +63,6 @@ export function Radar() {
           </div>
         );
       }}
-      legends={[
-        {
-          anchor: "top-left",
-          direction: "column",
-          translateX: -50,
-          translateY: -40,
-          itemWidth: 80,
-          itemHeight: 20,
-          itemTextColor: "#999",
-          symbolSize: 12,
-          symbolShape: "circle",
-          effects: [
-            {
-              on: "hover",
-              style: {
-                itemTextColor: "#000",
-              },
-            },
-          ],
-        },
-      ]}
     />
   );
 }
