b831fa0fe11fa0b25d28f3b69c7dc96b68027b6e
Ishi <syi378039@gmail.com>
2024-12-05 18:55:20 +0900

    帯グラフを追加

--- src/components/evaluation/Evaluation.jsx
+++ src/components/evaluation/Evaluation.jsx
@@ -2,6 +2,7 @@ import Grid from "@mui/material/Grid2";
 import Box from "@mui/material/Box";
 
 import Radar from "@/components/evaluation/radarChart.jsx";
+import RectangularGraph from "./rectangularGraph";
 
 export default function Evaluation(props) {
   const { works } = props;
@@ -27,15 +28,15 @@ export default function Evaluation(props) {
           <Radar works={works} />
         </Box>
       </Grid>
-      <Grid size="auto">
+      <Grid size="grow">
         <Box
           sx={{
             width: "100%",
-            height: "100%",
+            height: "90%",
             border: "1px solid #00f",
           }}
         >
-          <p>ここに帯グラフ</p>
+          <RectangularGraph works={works} />
         </Box>
       </Grid>
     </Grid>
