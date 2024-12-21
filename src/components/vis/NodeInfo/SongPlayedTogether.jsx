615d010c8213c91dbd75281e5f019e4a65c1e7f1
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2024-12-21 12:03:14 +0900

    生没年表示のフォーマットを修正し、スペースを削除

--- src/components/vis/NodeInfo/SongPlayedTogether.jsx
+++ src/components/vis/NodeInfo/SongPlayedTogether.jsx
@@ -56,7 +56,7 @@ const SongPlayedTogether = (props) => {
             <Typography variant="body2" component="div">
               {`${work.composer} ${
                 work.birth || work.death
-                  ? ` (${work.birth || ""} 〜 ${work.death || ""})`
+                  ? ` (${work.birth || ""}〜${work.death || ""})`
                   : ""
               }`}
             </Typography>
