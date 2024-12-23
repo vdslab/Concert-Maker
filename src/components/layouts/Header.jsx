26e2b595e4fe77f5b9cb953c588017d9b9ce1722
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2024-12-23 16:13:50 +0900

    ResponsiveAppBarコンポーネントのスタイルを改善

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -34,115 +34,31 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <AppBar position="sticky">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              alignItems: "center",
-              // fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            <Icon sx={{ textAlign: "center", mr: "8px" }} fontSize={"large"}>
-              <img style={{ height: "100%" }} src="/logo.svg" alt="Logo" />
-            </Icon>
-            Orchestra Concert Maker
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {navItems.map(({ item, link, target }) => (
-                <MenuItem key={item} onClick={handleCloseNavMenu}>
-                  <Button
-                    style={{ textAlign: "center" }}
-                    key={item}
-                    component="a"
-                    href={link}
-                    target={target}
-                  >
-                    {item}
-                  </Button>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <MusicNoteIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              // fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            Orchestra Concert Maker
-          </Typography>
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "none", md: "flex", justifyContent: "flex-end" },
-            }}
-          >
-            {navItems.map(({ item, link, target }) => (
-              <Button
-                key={item}
-                component="a"
-                href={link}
-                target={target}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {item}
-              </Button>
-            ))}
-          </Box>
-        </Toolbar>
-      </Container>
+    <AppBar position="sticky" sx={{ px: 2 }}>
+      <Toolbar disableGutters>
+        <Typography
+          variant="h6"
+          noWrap
+          component="a"
+          href="/"
+          sx={{
+            mr: 2,
+            display: { xs: "flex", md: "flex" },
+            alignItems: "center",
+            // fontFamily: "monospace",
+            fontWeight: 700,
+            letterSpacing: ".3rem",
+            color: "inherit",
+            textDecoration: "none",
+            textOverflow: "ellipsis",
+          }}
+        >
+          <Icon sx={{ textAlign: "center", mr: "8px" }} fontSize={"large"}>
+            <img style={{ height: "100%" }} src="/logo.svg" alt="Logo" />
+          </Icon>
+          Orchestra Concert Maker
+        </Typography>
+      </Toolbar>
     </AppBar>
   );
 }
