4921067b6d8f8c520c0f5133d893a382f576481e
Shirashoji <46389417+Shirashoji@users.noreply.github.com>
2024-12-23 16:14:09 +0900

    Headerコンポーネントの不要なインポートとコードを削除

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,38 +1,10 @@
-import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Icon from "@mui/material/Icon";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
-import MusicNoteIcon from "@mui/icons-material/MusicNote";
 
-const navItems = [
-  // { item: "Search", link: "/", target: "_self" },
-  // { item: "MyConcert", link: "/my-concert", target: "_self" },
-  // {
-  //   item: "Shirashoji",
-  //   link: "https://github.com/Shirashoji/",
-  //   target: "_blank",
-  // },
-];
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
   return (
     <AppBar position="sticky" sx={{ px: 2 }}>
       <Toolbar disableGutters>
